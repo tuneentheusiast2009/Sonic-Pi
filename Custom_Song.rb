4acc3a20d7bb072b9ebd0174e1e4f7866c3e2bcc
tuneentheusiast2009 <hppt5z6yjj@privaterelay.appleid.com>
2026-03-05 11:16:58 -0800

    Refactor live loops and adjust sample parameters

--- Custom_Song.rb
+++ Custom_Song.rb
@@ -1,64 +1,67 @@
 use_bpm 93
 use_synth :chipbass
-
 young = "C:/Users/aidan_arrington/Documents/Audacity/Young and Fine.wav"
 like = "C:/Users/aidan_arrington/Downloads/I Like Everything About You.wav"
 smiles = "C:/Users/aidan_arrington/Downloads/Gary Bartz - Gentle Smiles.wav"
 
+young_loops = 0
+
 live_loop :young do
   cue :sample_start
-  sample young, amp: 1.5, beat_stretch: 8
-  sleep 8
-  #sleep sample_duration(young)
-  #cue :sample_done
+  
+  young_loops += 1
+  
+  stop if young_loops > 8
+  
+  if young_loops <= 3
+    sample young, amp: 1.5, beat_stretch: 8
+    sleep 8
+    
+  elsif young_loops == 4
+    sample young, amp: 1.5, beat_stretch: 8, release: 4
+    sleep 8
+    
+  elsif young_loops <= 6
+    sleep 8
+    
+  else
+    sample young, amp: 0.5, beat_stretch: 8, attack: 2
+    sleep 8
+  end
 end
 
-sleep 16
-live_loop :pocket, sync: :sample_start do
-  sample :bd_haus, amp: 0.3
+live_loop :pocket, delay: 16, sync: :sample_start do
+  stop if young_loops > 6
+  
+  sample :bd_haus, amp: 0.2
   sleep 0.5
-  sample :bd_haus, amp: 0.3
+  sample :bd_haus, amp: 0.2
   sleep 0.5
-  sample :sn_generic, amp: 0.4
+  sample :sn_generic, amp: 0.3
   sleep 0.75
-  sample :bd_haus, amp: 0.3
+  sample :bd_haus, amp: 0.2
   sleep 0.50
-  sample :bd_haus, amp: 0.3
+  sample :bd_haus, amp: 0.2
   sleep 0.25
-  sample :bd_haus, amp: 0.3
+  sample :bd_haus, amp: 0.2
   sleep 0.5
-  sample :sn_generic, amp: 0.4
+  sample :sn_generic, amp: 0.3
   sleep 0.75
 end
 
-sleep 48
-live_loop :smiles do
-  sample smiles, amp: 1.5
-  sleep 8
-end
-
-sleep 32
-live_loop :pocket, sync: :sample_start do
-  sample :bd_haus, amp: 0.3
-  sleep 0.5
-  sample :bd_haus, amp: 0.3
-  sleep 0.5
-  sample :sn_generic, amp: 0.4
-  sleep 0.75
-  sample :bd_haus, amp: 0.3
-  sleep 0.50
-  sample :bd_haus, amp: 0.3
-  sleep 0.25
-  sample :bd_haus, amp: 0.3
-  sleep 0.5
-  sample :sn_generic, amp: 0.4
-  sleep 0.75
-end
-
-live_loop :hats do
+live_loop :hats, delay: 16 do
+  stop if young_loops > 6
+  
   2.times do
-    sample :drum_cymbal_closed, amp: 0.45
+    # sample :drum_cymbal_closed, amp: 0.45, rate: 0.9
     sleep 1
   end
+end
+
+live_loop :smiles, delay: 32, sync: :sample_start do
+  stop if young_loops > 6
+  
+  sample smiles, amp: 1
+  sleep 8
 end
 
